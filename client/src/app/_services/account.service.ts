7dee46487c02c7546c20654cbcaeef28e9290125
Pradeep Potnuri <pradeep.potnuri@selective.com>
2022-01-21 21:33:05 -0500

    End of section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -20,8 +20,7 @@ export class AccountService {
       map((response: User) => {
         const user = response;
         if(user){
-          localStorage.setItem('user',JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
@@ -30,16 +29,17 @@ export class AccountService {
   register(model:any){
     return this.http.post(this.baseUrl+'account/register',model).pipe(
       map((user: User)=>{
-        if(user){
-          localStorage.setItem('user',JSON.stringify(user));
-          this.currentUserSource.next(user);
+        if(user){          
+          this.setCurrentUser(user);
         }        
       })
     )
   }
-setCurrentUser(user: User) {
-  this.currentUserSource.next(user);
-}
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user',JSON.stringify(user));
+    this.currentUserSource.next(user);
+  }
   logout(){
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
