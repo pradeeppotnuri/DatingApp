221efd9a678468acbf01491ebf09d61b0ee2217c
Pradeep Potnuri <pradeep.potnuri@selective.com>
2022-01-23 22:39:17 -0500

    End of section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,10 +8,14 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required]
-        public string Username{get;set;}
+        [Required] public string Username{get;set;}
+        [Required] public string knownAs {get;set;}
+        [Required] public string Gender { get; set; }
+        [Required] public DateTime DateOfBirth { get; set; }
+        [Required] public string city { get; set; }
+        [Required] public string Country { get; set; }
         
-        [Required]
+        [Required] 
         [StringLength(8,MinimumLength =4)]
         public string Password{get;set;}
     }
